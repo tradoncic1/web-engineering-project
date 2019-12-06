e5a3c5c8e68f0f973674e8e70cc2f6555e02c024
tradoncic1 <tarik.r65@gmail.com>
2019-12-06 01:10:20 +0100

    add basic login form with request and add scss support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [input, setInput] = useState({
+    username: "",
+    password: ""
+  });
+  const [exists, setExists] = useState(null);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,26 +25,73 @@ const App = () => {
     fetchData();
   }, []);
 
+  const handleInput = event => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const login = async () => {
+    try {
+      const loginResponse = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: input.username,
+          password: input.password
+        })
+      });
+      const content = await loginResponse.json();
+      content ? setExists(true) : setExists(false);
+    } catch (error) {
+      console.log(error);
+      setExists(false);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="form-wrap">
+          <div className="form">
+            <div>
+              <label>Username</label>
+              <input
+                type="text"
+                name="username"
+                value={input.username}
+                onChange={handleInput}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                value={input.password}
+                onChange={handleInput}
+              />
+            </div>
+            {exists ? <div className="exists">This user exists!</div> : null}
+            <button onClick={login}>Log In</button>
+          </div>
+        </div>
+        <div
+          className="expand"
+          onClick={() => {
+            setExpanded(!expanded);
+          }}
         >
-          Learn React
-        </a>
-        <ul>
+          Expand To See Users
+        </div>
+        <ul style={expanded ? { height: "150px" } : { height: "0px" }}>
           {users.map((user, key) => {
             return (
               <li key={key}>
-                {user.firstName} {user.lastName}
+                {user.firstName} {user.lastName} | {user.username}:
+                {user.password}
               </li>
             );
           })}
