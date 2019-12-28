5112868dac3036790c5ed18aee77a57d0add1c07
tradoncic1 <tarik.r65@gmail.com>
2019-12-28 23:07:57 +0100

    heroku static content fix attempt 3

--- api/index.js
+++ api/index.js
@@ -24,8 +24,14 @@ app.use(bodyParser.json());
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-app.use("/", express.static("../frontend/build"));
-app.use(express.static("../frontend/build"));
+// app.use("/", express.static("../frontend/build"));
+if (__dirname.slice(-6) === "/build") {
+  // For production
+  app.use(express.static(__dirname));
+} else {
+  // For development
+  app.use(express.static("../frontend/build"));
+}
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
